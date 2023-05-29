3bbe994433af4e8dc2e5c58808a4595504730f8f
AldiyarIkhsan <usoul31@gmail.com>
2023-05-29 19:48:03 +0500

    Initial commit

---
+++ authentication/google_auth.py
@@ -0,0 +1,9 @@
+# from google.oauth2 import service_account
+#
+# SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+#
+# def get_gmail_credentials():
+#     credentials = service_account.Credentials.from_service_account_file(
+#         'path/to/your/service_account_key.json', scopes=SCOPES
+#     )
+#     return credentials
