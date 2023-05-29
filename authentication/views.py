b5b40d25a801a184cf7f9ded988f1eb2566807db
AldiyarIkhsan <usoul31@gmail.com>
2023-05-29 21:31:16 +0500

    Initial commit

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, status
-from authentication.serializers import RegisterSerializer
+from rest_framework import generics, status, views
+from authentication.serializers import RegisterSerializer, EmailVerificationSerializer
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from authentication.models import User
@@ -9,7 +9,8 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 import jwt
 from django.conf import settings
-
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class RegisterView(generics.GenericAPIView):
 
@@ -35,17 +36,22 @@ class RegisterView(generics.GenericAPIView):
         Util.send_email(data)
         return Response(user_data, status=status.HTTP_201_CREATED)
 
-    class VerifyEmail(generics.GenericAPIView):
-        def get(self, request):
-            token = request.Get.get('token')
-            try:
-                payload = jwt.decode(token, settings.SECRET_KEY)
-                user = User.objects.get(id=payload['user_id'])
-                if not user.is_verified:
-                    user.is_verified = True
-                    user.save()
-                return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
-            except jwt.ExpiredSignatureError as identifier:
-                return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
-            except jwt.exceptions.DecodeError as identifier:
-                return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+class VerifyEmail(views.APIView):
+    serializer_class = EmailVerificationSerializer
+
+    token_param_config = openapi.Parameter('token', in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
+
+    @swagger_auto_schema(manual_parameters=[token_param_config])
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+            user = User.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
